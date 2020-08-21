5040f3647aae21b9f38fefcebcd1acd895c60b7a
phuongnam8290 <phuongnam8290@gmail.com>
2020-08-21 12:51:48 +0700

    Add Spring Context Example

--- src/main/java/com/azure/dependencyinjectionexamples/DependencyInjectionExamplesApplication.java
+++ src/main/java/com/azure/dependencyinjectionexamples/DependencyInjectionExamplesApplication.java
@@ -1,13 +1,19 @@
 package com.azure.dependencyinjectionexamples;
 
+import com.azure.dependencyinjectionexamples.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionExamplesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DependencyInjectionExamplesApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DependencyInjectionExamplesApplication.class, args);
+        MyController myController = ctx.getBean("myController", MyController.class);
+
+        String greeting = myController.sayHello();
+        System.out.println(greeting);
     }
 
 }
---
+++ src/main/java/com/azure/dependencyinjectionexamples/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.azure.dependencyinjectionexamples.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello World!!");
+
+        return "Hi Folks!";
+    }
+}

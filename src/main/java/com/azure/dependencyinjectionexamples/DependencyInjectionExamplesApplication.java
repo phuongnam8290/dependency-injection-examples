d73c59c8cef004d101a2132d1e0bda15ab8ca808
phuongnam8290 <phuongnam8290@gmail.com>
2020-08-21 12:15:21 +0700

    Initialize Project Add

---
+++ src/main/java/com/azure/dependencyinjectionexamples/DependencyInjectionExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.azure.dependencyinjectionexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azure/dependencyinjectionexamples/DependencyInjectionExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.dependencyinjectionexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
